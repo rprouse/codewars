f1451d315124028a5b5538a667078896b7bd7b58
Rob Prouse <rob.prouse@aspiresoftware.com>
2023-09-12 11:43:35 -0400

    Solved sum of number series

---
+++ csharp/katas/SumOfNumbers.cs
@@ -0,0 +1,12 @@
+namespace codewars;
+
+public class SumOfNumbers
+{
+    public int GetSum(int a, int b)
+    {
+        var min = Math.Min(a, b);
+        var max = Math.Max(a, b);
+
+        return Enumerable.Range(min, max - min + 1).Sum();
+    }
+}
