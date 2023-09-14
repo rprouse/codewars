a1f36cd36ea7fd7ea127912aeb31971c6efc41cd
Rob Prouse <rob.prouse@aspiresoftware.com>
2023-09-14 11:03:29 -0400

    Binary addition

---
+++ csharp/katas/BinaryAddtion.cs
@@ -0,0 +1,7 @@
+﻿namespace codewars;
+
+public class BinaryAddtion
+{
+    public static string AddBinary(int a, int b) =>
+        Convert.ToString(a + b, 2);
+}
---
+++ csharp/tests/BinaryAddtionTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+using Shouldly;
+
+namespace codewars;
+
+public class BinaryAddtionTests
+{
+    [TestCase(1, 2, "11")]
+    [TestCase(5, 9, "1110")]
+    [TestCase(10, 10, "10100")]
+    [TestCase(0, 0, "0")]
+    [TestCase(0, 1, "1")]
+    [TestCase(1, 0, "1")]
+    public void AddBinary(int a, int b, string expected)
+    {
+        BinaryAddtion.AddBinary(a, b).ShouldBe(expected);
+    }
+}
