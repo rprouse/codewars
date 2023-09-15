0185677967b9b4557151aafe6ddf9c7064aabfa9
Rob Prouse <rob.prouse@aspiresoftware.com>
2023-09-15 10:58:22 -0400

    Simple solution to Human Readable Time

---
+++ csharp/katas/HumanReadableTime.cs
@@ -0,0 +1,15 @@
+﻿namespace codewars;
+
+public class HumanReadableTime
+{
+    public static string GetReadableTime(int seconds)
+    {
+        if (seconds < 0 || seconds > 359999)
+            throw new ArgumentOutOfRangeException(nameof(seconds), "Must be between 0 and 359999");
+
+        var hrs  = seconds / 3600;
+        var mins = (seconds - hrs * 3600) / 60;
+        var secs = seconds - hrs * 3600 - mins * 60;
+        return $"{hrs:00}:{mins:00}:{secs:00}";
+    }
+}
---
+++ csharp/tests/HumanReadableTimeTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+using Shouldly;
+
+namespace codewars;
+
+public class HumanReadableTimeTests
+{
+    [TestCase(0, "00:00:00")]
+    [TestCase(5, "00:00:05")]
+    [TestCase(60, "00:01:00")]
+    [TestCase(86399, "23:59:59")]
+    [TestCase(359999, "99:59:59")]
+    public void ShouldFormatCorrectly(int seconds, string expected)
+    {
+        HumanReadableTime.GetReadableTime(seconds).ShouldBe(expected);
+    }
+
+    [TestCase(-1)]
+    [TestCase(360000)]
+    public void ShouldThrowException(int seconds)
+    {
+        Should.Throw<ArgumentOutOfRangeException>(() => HumanReadableTime.GetReadableTime(seconds));
+    }
+}
