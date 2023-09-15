9d3a2fb4a0438d8cc928f0f15ce23f4516a89ed0
Rob Prouse <rob.prouse@aspiresoftware.com>
2023-09-15 11:13:21 -0400

    A better implementation of human readable

--- csharp/katas/HumanReadableTime.cs
+++ csharp/katas/HumanReadableTime.cs
@@ -8,8 +8,8 @@ public class HumanReadableTime
             throw new ArgumentOutOfRangeException(nameof(seconds), "Must be between 0 and 359999");
 
         var hrs  = seconds / 3600;
-        var mins = (seconds - hrs * 3600) / 60;
-        var secs = seconds - hrs * 3600 - mins * 60;
+        var mins = (seconds % 3600) / 60;
+        var secs = seconds % 60;
         return $"{hrs:00}:{mins:00}:{secs:00}";
     }
 }
