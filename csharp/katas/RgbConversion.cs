42b903edd7712824bf18783b3bfe66a3c0b69803
Rob Prouse <rob.prouse@aspiresoftware.com>
2023-09-18 13:10:42 -0400

    Convert to RGB Kata

---
+++ csharp/katas/RgbConversion.cs
@@ -0,0 +1,15 @@
+﻿namespace codewars;
+
+public static class RgbConversion
+{
+    public static string Rgb(int r, int g, int b) => 
+        $"{ToHex(r)}{ToHex(g)}{ToHex(b)}";
+
+    private static string ToHex(int value) =>
+        value switch
+        {
+            < 0 => "00",
+            > 255 => "FF",
+            _ => value.ToString("X2")
+        };
+}
---
+++ csharp/tests/RgbConversionTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+using Shouldly;
+
+namespace codewars;
+
+public class RgbConversionTests
+{
+    [TestCase(255, 255, 255, "FFFFFF")]
+    [TestCase(255, 255, 300, "FFFFFF")]
+    [TestCase(0, 0, 0, "000000")]
+    [TestCase(148, 0, 211, "9400D3")]
+    [TestCase(148, -10, 211, "9400D3")]
+    public void CanConvertToRgb(int r, int g, int b, string expected)
+    {
+        RgbConversion.Rgb(r, g, b).ShouldBe(expected);
+    }
+}
+
