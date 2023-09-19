f5acbee0cde324537a95db3ca4d53e439f4cfeb7
Rob Prouse <rob.prouse@aspiresoftware.com>
2023-09-19 13:42:47 -0400

    Scramble solved

---
+++ csharp/katas/Scramblies.cs
@@ -0,0 +1,8 @@
+﻿namespace codewars;
+
+public class Scramblies
+{
+    public static bool Scramble(string str1, string str2) =>
+        str2.Distinct()
+            .All(c => str1.Count(c1 => c1 == c) >= str2.Count(c2 => c2 == c));
+}
---
+++ csharp/tests/ScrambliesTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+using Shouldly;
+
+namespace codewars;
+
+public static class ScrambliesTests
+{
+    [TestCase("rkqodlw", "world", true)]
+    [TestCase("cedewaraaossoqqyt", "codewars", true)]
+    [TestCase("katas", "steak", false)]
+    [TestCase("scriptjavx", "javascript", false)]
+    [TestCase("scriptingjava", "javascript", true)]
+    [TestCase("scriptsjava", "javascripts", true)]
+    [TestCase("javscripts", "javascript", false)]
+    [TestCase("aabbcamaomsccdd", "commas", true)]
+    [TestCase("commas", "commas", true)]
+    [TestCase("sammoc", "commas", true)]
+    public static void CanFindScrambledWord(string str1, string str2, bool expected)
+    {
+        Scramblies.Scramble(str1, str2).ShouldBe(expected);
+    }
+}
