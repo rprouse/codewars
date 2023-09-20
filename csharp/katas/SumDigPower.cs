539b6c633e500cb315588b5a07fb414c528be2f7
Rob Prouse <rob.prouse@aspiresoftware.com>
2023-09-20 16:17:07 -0400

    Solved SumDigPower

---
+++ csharp/katas/SumDigPower.cs
@@ -0,0 +1,28 @@
+﻿namespace codewars;
+
+public class SumDigPower
+{
+    public static long[] SumDigPow(long a, long b)
+    {
+        var result = new List<long>();
+        for (var i = a; i <= b; i++)
+        {
+            if (IsSumDigPow(i))
+            {
+                result.Add(i);
+            }
+        }
+        return result.ToArray();
+    }
+
+    private static bool IsSumDigPow(long i)
+    {
+        var digits = i.ToString().Select(c => char.GetNumericValue(c)).ToArray();
+        var sum = 0;
+        for (var j = 0; j < digits.Length; j++)
+        {
+            sum += (int)Math.Pow(digits[j], j + 1);
+        }
+        return sum == i;
+    }
+}
---
+++ csharp/tests/SumDigPowerTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+using Shouldly;
+
+namespace codewars;
+
+public class SumDigPowerTests
+{
+    [TestCaseSource(nameof(SumDigPowerTestData))]
+    public void CanFindSumDigPowers(long a, long b, long[] res)
+    {
+        SumDigPower.SumDigPow(a, b).ShouldBe(res);
+    }
+
+    public static IEnumerable<TestCaseData> SumDigPowerTestData => new[]
+    {
+        new TestCaseData(1, 10, new long[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }),
+        new TestCaseData(1, 100, new long[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 89 }),
+        new TestCaseData(10, 100, new long[] { 89 }),
+        new TestCaseData(90, 100, new long[] { }),
+        new TestCaseData(90, 150, new long[] { 135 }),
+        new TestCaseData(50, 150, new long[] { 89, 135 }),
+        new TestCaseData(10, 150, new long[] { 89, 135 })
+    };
+}
