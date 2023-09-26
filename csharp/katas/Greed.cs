b26384ae207f7a4a82898d3b00186dd65005e62a
Rob Prouse <rob.prouse@aspiresoftware.com>
2023-09-26 12:54:33 -0400

    Score the game of greed

---
+++ csharp/katas/Greed.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+namespace codewars;
+
+public class Greed
+{
+    public readonly static int[] Scores = new int[] { 0, 1000, 200, 300, 400, 500, 600 };
+
+    public static int Score(int[] dice)
+    {
+        int score = Enumerable.Range(1, 6).Sum(i => dice.Count(die => die == i) >= 3 ? Scores[i] : 0);
+        if (dice.Count(dice => dice == 1) >= 3)
+        {
+            dice = dice.OrderByDescending(die => die != 1).Take(2).ToArray();
+        }
+        if (dice.Count(dice => dice == 5) >= 3)
+        {
+            dice = dice.OrderByDescending(die => die != 5).Take(2).ToArray();
+        }
+        score += dice.Where(dice => dice == 1).Count() * 100;
+        score += dice.Where(dice => dice == 5).Count() * 50;
+        return score;
+    }
+}
---
+++ csharp/tests/GreedTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using Shouldly;
+
+namespace codewars;
+
+public class GreedTests
+{
+    [TestCaseSource(nameof(GreedScores))]
+    public void CanScore(int[] dice, int score)
+    {
+        Greed.Score(dice).ShouldBe(score);
+    }
+
+    public static IEnumerable<TestCaseData> GreedScores => new [] 
+    {
+        new TestCaseData(new int[] { 2, 3, 4, 6, 2 }, 0),
+        new TestCaseData(new int[] { 4, 4, 4, 3, 3 }, 400),
+        new TestCaseData(new int[] { 2, 4, 4, 5, 4 }, 450),
+        new TestCaseData(new int[] { 5, 1, 3, 4, 1 }, 250),
+        new TestCaseData(new int[] { 1, 1, 1, 3, 1 }, 1100),
+        new TestCaseData(new int[] { 1, 1, 1, 1, 1 }, 1200),
+        new TestCaseData(new int[] { 5, 5, 5, 5, 4 }, 550),
+        new TestCaseData(new int[] { 5, 5, 5, 5, 5 }, 600),
+    };
+}
