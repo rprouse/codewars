06ce604cbe775ce3ab5b8656237866300ad78783
Rob Prouse <rob.prouse@aspiresoftware.com>
2023-09-26 12:17:01 -0400

    Move zeroes kata

---
+++ csharp/katas/MoveZeros.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace codewars;
+
+public class MoveZeros
+{
+    public static int[] MoveZeroes(int[] arr)
+    {
+        int zeros = arr.Count(x => x == 0);
+        return arr.Where(x => x != 0)
+            .Concat(Enumerable.Repeat(0, zeros))
+            .ToArray();
+    }
+}
---
+++ csharp/tests/MoveZerosTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+using Shouldly;
+
+namespace codewars;
+
+public class MoveZerosTests
+{
+    [Test]
+    public void CanMoveZeroes()
+    {
+        MoveZeros.MoveZeroes(new int[] { 1, 2, 0, 1, 0, 1, 0, 3, 0, 1 })
+            .ShouldBe(new int[] { 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 });
+    }
+}
